7753ecc8470769cfc1e79e94e59a07f42f15bc54
Yogendra Manawat <yogendramanawat@gmail.com>
2023-10-14 11:53:09 +0530

    some changes to host on netlify

--- src/index.js
+++ src/index.js
@@ -1,60 +1,74 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-const { generateMessage } = require('./utils/messages')
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const { generateMessage } = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 5000
-const publicDirectoryPath = path.join(__dirname, './public')
+const port = process.env.PORT || 5000;
+const publicDirectoryPath = path.join(__dirname, "./public");
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
+app.use("/.netlify/functions/api", router);
 
-io.on('connection', (socket) => {
-    console.log('New connection established')
+io.on("connection", (socket) => {
+  console.log("New connection established");
 
-    socket.on('join', (options, callback) => {
-        const { error, user } = addUser({ id: socket.id, ...options })
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
 
-        if (error) {
-            return callback(error)
-        }
+    if (error) {
+      return callback(error);
+    }
 
-        socket.join(user.room)
+    socket.join(user.room);
 
-        socket.emit('message', generateMessage('Admin', 'Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
-        io.to(user.room).emit('roomData', {
-            room: user.room,
-            users: getUsersInRoom(user.room)
-        })
+    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        generateMessage("Admin", `${user.username} has joined!`)
+      );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
-        callback()
-    })
+    callback();
+  });
 
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit('message', generateMessage(user.username, message))
-        callback()
-    })
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", generateMessage(user.username, message));
+    callback();
+  });
 
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id)
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
 
-        if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
-            io.to(user.room).emit('roomData', {
-                room: user.room,
-                users: getUsersInRoom(user.room)
-            })
-        }
-    })
-})
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage("Admin", `${user.username} has left!`)
+      );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}!`)
-})
+  console.log(`Server is up on port ${port}!`);
+});
